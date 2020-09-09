ea7595a9f11d53113f7793276a5c225fe772742d
Mohammed Kamal <51802233+mkamaldev@users.noreply.github.com>
2020-09-09 10:08:35 +0100

    Initial commit

---
+++ src/components/404.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="not-found">
+        <h3>404 Not Found</h3>
+        <a className="goback" href="/">Back Home</a>
+        </div>
+    )
+  }
+}
